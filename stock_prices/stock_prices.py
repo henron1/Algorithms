c20106345454df57acd6740717153d42f265122d
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-05-01 16:27:38 -0400

    stock prices, moving on to recipe batches

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,23 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  min_price_now = None
-  max_profit = None
-  for i in range(len(prices)):
-    current_price = prices[i]
-    if min_price_now:
-      if current_price > min_price_now:
-        max_profit = current_price - min_price_now
-      else:
-        min_price_now = current_price
-    else:
-      min_price_now = current_price
-  return max_profit
-# print(find_max_profit([10, 7, 5, 8, 11, 9]))
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
+  maxProfit = 0 #setting intial maximum profit
+  for i in range(0, len(prices)): #looping over outer scope of prices
+    for j in range(prices.index(prices[i]) + 1, len(prices)): #inner scope of prices that is 1 ahead of the outer scope price
+      profit = prices[j] - prices[i] #setting up remainder of profit
+      print(profit) 
+      if maxProfit == 0: # if there's no max profit then it is set to the previous remainder
+        maxProfit = profit
+      elif profit > maxProfit: # if there's no max profit then swap
+        swap = profit
+        maxProfit = swap
+  return maxProfit
 
-  # print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
+
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
